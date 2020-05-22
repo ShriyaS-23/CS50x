0ae4c0fcf8ae573882cc58ba5c22d5016d583aef
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-05-22 13:21:44 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -1,44 +1,63 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
+long number(void);
+
+int main (void)
 {
-    long n = get_long("Card Number:");
+    long n = number();
     long x = n;
-    long s = 0;
-    long k = 0;
-    while(n!=0)
+    long q = n;
+    long t = n;
+    long y = 0;
+    while (x != 0)
     {
-        n = n/10;;
-        long c = n%10;
-        n = n/10;
-        if (c/10 == 0)
-        {
-            s = s + c*2;
-        }
-        else
-        {
-            while (c!=0)
-            {
-                long d = c%10;
-                s = s + d;
-                c = c/10;
-            }
-        }
+        x = n%100;
+        y = y + (x * 2);
+        n = n/100;
     }
-    while(x!=0)
+    long s1 = 0;
+    while (y != 0)
     {
-        long c = x%10;
-        x = n/100;
-        k = k + c;
+        s1 = s1 + y%10;
+        y = y/10;
     }
-    long ans = k + s;
-    if (ans%10 == 0)
+    
+    long s2 = 0;
+    while (q != 0)
     {
-        printf("MASTERCARD\n");
+        s2 = s2 + q%10;
+        q = q/10;
+    }
+    
+    long s = s1 + s2;
+    
+    if (s%10 == 0 && (t/10000000000000 == 34 || t/10000000000000 == 37))
+    {
+        printf("American Express");
+    }
+    else if (s%10 == 0 && (t/1000000000000000 == 51 || t/1000000000000000 == 52 || t/1000000000000000 == 53 || t/1000000000000000 == 54 || t/1000000000000000 == 55))
+    {
+        printf("Mastercard");
+    }
+    else if (s%10 == 0 && (t/100000000000 == 4 || t/100000000000000 == 4))
+    {
+        printf("Visa");
     }
     else
     {
-        printf("INVALID\n");
+        printf("Invalid");
     }
+    
+}
+
+long number(void)
+{
+    long n = 0;
+    do
+    {
+        n = get_long("Card Number: ");
+    }
+    while(n<0);
+    return n;
 }
