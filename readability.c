4165cd8065f5a3d2cc68f810eb38eb03245195a9
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-05-23 13:17:08 +0000

    automated commit by check50 [check50=True]

---
+++ readability.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <math.h>
+
+int count_letters(string t);
+int count_words(string t);
+int count_sentences(string t);
+
+int main(void)
+{
+    string text = get_string("Text :\n");
+    int l = count_letters(text);
+    int w = count_words(text);
+    int s = count_sentences(text);
+    
+    int read = round (0.0588 * l - 0.296 * s - 15.8);
+    
+    if (read) < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (read > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", read);
+    }
+    
+}
+
+int count_letters(string t)
+{
+    int n = 0;
+    for(int i = 0, x = strlen(t); i <= x; i++)
+    {
+        if ((t[i] >= 'a' && t[i] <= 'z') || (t[i] >= 'A' && t[i] <= 'Z'))
+        {
+            n++;
+        }
+    }
+    return n;
+}
+
+int count_words(string t)
+{
+    int n = 0;
+    for(int i = 0, x = strlen(t); i <= x; i++)
+    {
+        if (t[i] == ' ')
+        {
+            n++;
+        }
+    }
+    return n+1;
+}
+
+int count_sentences(string t)
+{
+    int n = 0;
+    for (int i = 0, x = strlen(t); i <= x; i++)
+    {
+        if (t[i] == '.' || t[i] == '!' || t[i] == '?')
+        {
+            n++;
+        }
+    }
+    return n;
+}
