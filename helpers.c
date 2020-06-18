b361659fa2eaccdc78fa16d150993c6b7596dd72
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-06-18 15:55:03 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,259 +1,139 @@
 #include "helpers.h"
-#include "math.h"
-#include "stdio.h"
+#include<math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
+
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            // get average of rgb value of each pixel
-            int b = image[i][j].rgbtBlue;
-            int g = image[i][j].rgbtGreen;
-            int r = image[i][j].rgbtRed;
-            float avgr = (float)(r + g + b) / 3;
-            int avg = round(avgr);
-            // set avg for rgb values
-            image[i][j].rgbtBlue = avg;
-            image[i][j].rgbtGreen = avg;
-            image[i][j].rgbtRed = avg;
-        }
-    }
-    return;
+    float avg;
+    for(int i=0;i<height;i++) // loop thru rows top to bottom
+{
+for(int j=0;j<width;j++) // loop thru columns left to right
+{
+   avg=round(((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3.00));
+
+image[i][j].rgbtRed=avg;
+image[i][j].rgbtBlue=avg;
+image[i][j].rgbtGreen=avg;
+ }
+
+}
+}
+
+// hit max limit
+int limit(int rgb)
+  {
+if(rgb>255)
+{
+    rgb=255;
+}
+return rgb;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            // use sepia algorithim
-            float sr = (float) .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
-            float sg = (float) .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
-            float sb = (float) .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
-            // round values
-            int rsr = round(sr);
-            int rsg = round(sg);
-            int rsb = round(sb);
-            // make sure it doesnt go over 255
-            if (rsr > 255)
-            {
-                rsr = 255;
-            }
-            if (rsg > 255)
-            {
-                rsg = 255;
-            }
-            if (rsb > 255)
-            {
-                rsb = 255;
-            }
-            // set values as rgb values
-            image[i][j].rgbtRed = rsr;
-            image[i][j].rgbtGreen = rsg;
-            image[i][j].rgbtBlue = rsb;
-        }
-    }
-    return;
+for(int i=0;i<height;i++) // loop thru rows top to bottom
+{
+for(int j=0;j<width;j++) // loop thru columns left to right
+{ int sepiaRed,sepiaGreen,sepiaBlue;
+
+sepiaRed =limit( round (.393 *image[i][j].rgbtRed  + .769 *image[i][j].rgbtGreen  + .189 * image[i][j].rgbtBlue));
+  sepiaGreen =limit(round (.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue));
+  sepiaBlue = limit(round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue));
+  
+    image[i][j].rgbtRed=sepiaRed;
+image[i][j].rgbtBlue=sepiaBlue;
+image[i][j].rgbtGreen=sepiaGreen;
+}
+}
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
+{ RGBTRIPLE t;
+
+    for(int i=0;i<height;i++) // loop thru rows top to bottom
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < (width / 2); j++)
-        {
-            RGBTRIPLE ib = image[i][j];
-            image[i][j] = image[i][width - (j + 1)];
-            image[i][width - (j + 1)] = ib;
-        }
-    }
-    return;
+    int n=(width);
+    for(int j = 0; j <n/2; j++) // loop thru columns left to right until mid column
+ {
+     {
+         int k=n-j; // last pixel-j gives the loop of pixels from right to mid column
+      { // alterate :t=image[i][j];
+     // image[i][j]=image[i][k-1];
+      //image[i][k-1]=t;
+        t[0]=image[i][j].rgbtRed;
+    t[1]=image[i][j].rgbtGreen;
+    t[2]=image[i][j].rgbtBlue;
+
+    image[i][j].rgbtRed= image[i][k-1].rgbtRed;
+    image[i][j].rgbtGreen= image[i][k-1].rgbtGreen;
+    image[i][j].rgbtBlue= image[i][k-1].rgbtBlue;
+
+   image[i][k-1].rgbtRed=t[0];
+  image[i][k-1].rgbtGreen=t[1];
+  image[i][k-1].rgbtBlue=t[2];
+    
+  }
+ }
+}
+}
 }
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE original[height][width])
+void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    //set rgbtriple copy as a copy of original
-    RGBTRIPLE copy[height][width];
-    for (int y = 0; y < height; y++)
+
+    int sumblue;
+    int sumgreen;
+    int sumred;
+    float counter;
+    //create a temporary table of colors to not alter the calculations
+    RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
     {
-        for (int x = 0; x < width; x++)
+        for (int j = 0; j < width; j++)
         {
-            copy[y][x] = original[y][x];
+            sumblue = 0;
+            sumgreen = 0;
+            sumred = 0;
+            counter = 0.00;
+
+            // sums values of the pixel and 8 neighboring ones, skips iteration if it goes outside the pic
+           for(int m=-1;m<2;m++)  // loop thru row of pixel matrix 3x3 -1 0 1
+           {
+               for(int n=-1;n<2;n++) // loop thru column of pixel matrix 3x3 -1 0 1
+          {
+              if(i+m<0 || i+m>height-1) // to eliminate edges beyond the image above or bottom, if i=0 m=-1 then eliminate
+           { continue; // like a break statement, moves to next iteration
+           }
+           if(j+n<0 || j+m>width-1) // to elimiate pixels beyond left and right boundary of image width
+           { continue;
+           } // the pixels which lie in image matrix will be added per rgb values
+           sumblue+=image[i+m][j+n].rgbtBlue; // sum of blue values of all pixels in 3x3 for that particular pixel looped into m,n and then i,j
+           sumgreen+=image[i+m][j+n].rgbtGreen;
+           sumred+=image[i+m][j+n].rgbtRed;
+           counter++; // after the rbg values of that particular pixel is added(adding for neighboring values of it) increase counter  and move to next m,n and then i,j
+
+
+    // take avg by sum/ counter and store it in temp rgbtriple
+    temp[i][j].rgbtBlue=round(sumblue/counter);
+    temp[i][j].rgbtGreen=round(sumgreen/counter);
+    temp[i][j].rgbtRed=round(sumred/counter);
+          }
+           }
+        }
+    } // we take a copy in temp so that the values for imageIJ dont change as loop proceeds
+    // copy that into actual pixel for every pixel from all values of i and j
+     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+     image[i][j].rgbtBlue=temp[i][j].rgbtBlue;
+     image[i][j].rgbtGreen=temp[i][j].rgbtGreen;
+     image[i][j].rgbtRed=temp[i][j].rgbtRed;
         }
     }
-
-    //checking individual pixels
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            //checking for corners
-
-            // checking top left corner
-            if (y == 0 && x == 0)
-            {
-                //averaging neighbors
-                float ar = round((float)(copy[1][1].rgbtRed + copy[0][1].rgbtRed + copy[1][0].rgbtRed + copy[0][0].rgbtRed) / 4);
-                float ab = round((float)(copy[1][1].rgbtBlue + copy[0][1].rgbtBlue + copy[1][0].rgbtBlue + copy[0][0].rgbtBlue) / 4);
-                float ag = round((float)(copy[1][1].rgbtGreen + copy[0][1].rgbtGreen + copy[1][0].rgbtGreen + copy[0][0].rgbtGreen) / 4);
-
-                //setting avgs as pixel values
-                original[0][0].rgbtRed = ar;
-                original[0][0].rgbtBlue = ab;
-                original[0][0].rgbtGreen = ag;
-            }
-            // checking top right corner
-            else if (y == 0 && x == width - 1)
-            {
-                //averaging neighbors
-                float ar = round((float)(copy[1][width - 2].rgbtRed + copy[0][width - 2].rgbtRed + copy[1][width - 1].rgbtRed + copy[0][width -
-                                         1].rgbtRed) / 4);
-                float ab = round((float)(copy[1][width - 2].rgbtBlue + copy[0][width - 2].rgbtBlue + copy[1][width - 1].rgbtBlue + copy[0][width -
-                                         1].rgbtBlue) / 4);
-                float ag = round((float)(copy[1][width - 2].rgbtGreen + copy[0][width - 2].rgbtGreen + copy[1][width - 1].rgbtGreen + copy[0][width
-                                         - 1].rgbtGreen) / 4);
-
-                //setting avgs as pixel values
-                original[0][width - 1].rgbtRed = ar;
-                original[0][width - 1].rgbtBlue = ab;
-                original[0][width - 1].rgbtGreen = ag;
-            }
-            // checking bottom left corner
-            else if (y == height - 1 && x == 0)
-            {
-                //averaging neighbors
-                float ar = round((float)(copy[height - 1][1].rgbtRed + copy[height - 2][1].rgbtRed + copy[height - 2][0].rgbtRed + copy[height -
-                                         1][0].rgbtRed) / 4);
-                float ab = round((float)(copy[height - 1][1].rgbtBlue + copy[height - 2][1].rgbtBlue + copy[height - 2][0].rgbtBlue + copy[height -
-                                         1][0].rgbtBlue) / 4);
-                float ag = round((float)(copy[height - 1][1].rgbtGreen + copy[height - 2][1].rgbtGreen + copy[height - 2][0].rgbtGreen + copy[height
-                                         - 1][0].rgbtGreen) / 4);
-
-                //setting avgs as pixel values
-                original[height - 1][0].rgbtRed = ar;
-                original[height - 1][0].rgbtBlue = ab;
-                original[height - 1][0].rgbtGreen = ag;
-            }
-            // checking bottom right corner
-            else if (y == height - 1 && x == width - 1)
-            {
-                //averaging neighbors
-                float ar = round((float)(copy[height - 2][width - 1].rgbtRed + copy[height - 1][width - 2].rgbtRed + copy[height - 2][width -
-                                         2].rgbtRed + copy[height - 1][width - 1].rgbtRed) / 4);
-                float ab = round((float)(copy[height - 2][width - 1].rgbtBlue + copy[height - 1][width - 2].rgbtBlue + copy[height - 2][width -
-                                         2].rgbtBlue + copy[height - 1][width - 1].rgbtBlue) / 4);
-                float ag = round((float)(copy[height - 2][width - 1].rgbtGreen + copy[height - 1][width - 2].rgbtGreen + copy[height - 2][width -
-                                         2].rgbtGreen + copy[height - 1][width - 1].rgbtGreen) / 4);
-
-                //setting avgs as pixel values
-                original[height - 1][width - 1].rgbtRed = ar;
-                original[height - 1][width - 1].rgbtBlue = ab;
-                original[height - 1][width - 1].rgbtGreen = ag;
-            }
-
-            //checking for edges
-
-            //checking for top edge
-            else if (y == 0 && x != 0 && x != width - 1)
-            {
-                float ar = round((float)(copy[y + 1][x - 1].rgbtRed + copy[y][x - 1].rgbtRed + copy[y][x + 1].rgbtRed + copy[y + 1][x + 1].rgbtRed +
-                                         copy[y + 1][x].rgbtRed + copy[y][x].rgbtRed) / 6);
-                float ab = round((float)(copy[y + 1][x - 1].rgbtBlue + copy[y][x - 1].rgbtBlue + copy[y][x + 1].rgbtBlue + copy[y + 1][x +
-                                         1].rgbtBlue +
-                                         copy[y + 1][x].rgbtBlue + copy[y][x].rgbtRed) / 6);
-                float ag = round((float)(copy[y + 1][x - 1].rgbtGreen + copy[y][x - 1].rgbtGreen + copy[y][x + 1].rgbtGreen + copy[y + 1][x +
-                                         1].rgbtGreen +
-                                         copy[y + 1][x].rgbtGreen + copy[y][x].rgbtRed) / 6);
-
-                //setting avgs as pixel values
-                original[y][x].rgbtRed = ar;
-                original[y][x].rgbtBlue = ab;
-                original[y][x].rgbtGreen = ag;
-            }
-            //checking for left edge
-            else if (y != 0 && y != height - 1 && x == 0)
-            {
-                float ar = round((float)(copy[y - 1][x].rgbtRed + copy[y - 1][x + 1].rgbtRed + copy[y][x + 1].rgbtRed + copy[y + 1][x + 1].rgbtRed +
-                                         copy[y + 1][x].rgbtRed + copy[y][x].rgbtRed) / 6);
-                float ab = round((float)(copy[y - 1][x].rgbtBlue + copy[y - 1][x + 1].rgbtBlue + copy[y][x + 1].rgbtBlue + copy[y + 1][x +
-                                         1].rgbtBlue +
-                                         copy[y + 1][x].rgbtBlue + copy[y][x].rgbtRed) / 6);
-                float ag = round((float)(copy[y - 1][x].rgbtGreen + copy[y - 1][x + 1].rgbtGreen + copy[y][x + 1].rgbtGreen + copy[y + 1][x +
-                                         1].rgbtGreen +
-                                         copy[y + 1][x].rgbtGreen + copy[y][x].rgbtRed) / 6);
-
-                //setting avgs as pixel values
-                original[y][x].rgbtRed = ar;
-                original[y][x].rgbtBlue = ab;
-                original[y][x].rgbtGreen = ag;
-            }
-            //checking for right edge
-            else if (y != 0 && y != height - 1 && x == width - 1)
-            {
-                float ar = round((float)(copy[y - 1][x].rgbtRed + copy[y - 1][x - 1].rgbtRed + copy[y][x - 1].rgbtRed + copy[y + 1][x - 1].rgbtRed +
-                                         copy[y + 1][x].rgbtRed + copy[y][x].rgbtRed) / 6);
-                float ab = round((float)(copy[y - 1][x].rgbtBlue + copy[y - 1][x - 1].rgbtBlue + copy[y][x - 1].rgbtBlue + copy[y + 1][x -
-                                         1].rgbtBlue +
-                                         copy[y + 1][x].rgbtBlue + copy[y][x].rgbtRed) / 6);
-                float ag = round((float)(copy[y - 1][x].rgbtGreen + copy[y - 1][x - 1].rgbtGreen + copy[y][x - 1].rgbtGreen + copy[y + 1][x -
-                                         1].rgbtGreen +
-                                         copy[y + 1][x].rgbtGreen + copy[y][x].rgbtRed) / 6);
-
-                //setting avgs as pixel values
-                original[y][x].rgbtRed = ar;
-                original[y][x].rgbtBlue = ab;
-                original[y][x].rgbtGreen = ag;
-            }
-            //checking for bottom edge
-            else if (y == height - 1 && x != width - 1 && x != 0)
-            {
-                float ar = round((float)(copy[y][x + 1].rgbtRed + copy[y - 1][x + 1].rgbtRed + copy[y][x - 1].rgbtRed + copy[y - 1][x - 1].rgbtRed +
-                                         copy[y - 1][x].rgbtRed + copy[y][x].rgbtRed) / 6);
-                float ab = round((float)(copy[y][x + 1].rgbtBlue + copy[y - 1][x + 1].rgbtBlue + copy[y][x - 1].rgbtBlue + copy[y - 1][x -
-                                         1].rgbtBlue +
-                                         copy[y - 1][x].rgbtBlue + copy[y][x].rgbtRed) / 6);
-                float ag = round((float)(copy[y][x + 1].rgbtGreen + copy[y - 1][x + 1].rgbtGreen + copy[y][x - 1].rgbtGreen + copy[y - 1][x -
-                                         1].rgbtGreen +
-                                         copy[y - 1][x].rgbtGreen + copy[y][x].rgbtRed) / 6);
-
-                //setting avgs as pixel values
-                original[y][x].rgbtRed = ar;
-                original[y][x].rgbtBlue = ab;
-                original[y][x].rgbtGreen = ag;
-            }
-
-
-            //checking everything else
-            else
-            {
-                float ar = round((float)(copy[y - 1][x - 1].rgbtRed + copy[y - 1][x].rgbtRed + copy[y - 1][x + 1].rgbtRed + copy[y][x - 1].rgbtRed +
-                                         copy[y + 1][x + 1].rgbtRed + copy[y][x + 1].rgbtRed + copy[y + 1][x - 1].rgbtRed + copy[y + 1][x].rgbtRed +
-                                         copy[y][x].rgbtRed) /
-                                 9);
-                float ab = round((float)(copy[y - 1][x - 1].rgbtBlue + copy[y - 1][x].rgbtBlue + copy[y - 1][x + 1].rgbtBlue + copy[y][x -
-                                         1].rgbtBlue +
-                                         copy[y + 1][x + 1].rgbtBlue + copy[y][x + 1].rgbtBlue + copy[y + 1][x - 1].rgbtBlue + copy[y + 1][x].rgbtBlue +
-                                         copy[y][x].rgbtBlue) /
-                                 9);
-                float ag = round((float)(copy[y - 1][x - 1].rgbtGreen + copy[y - 1][x].rgbtGreen + copy[y - 1][x + 1].rgbtGreen + copy[y][x -
-                                         1].rgbtGreen +
-                                         copy[y + 1][x + 1].rgbtGreen + copy[y][x + 1].rgbtGreen + copy[y + 1][x - 1].rgbtGreen + copy[y + 1][x].rgbtGreen +
-                                         copy[y][x].rgbtGreen) /
-                                 9);
-
-                //setting avgs as pixel values
-                original[y][x].rgbtRed = ar;
-                original[y][x].rgbtBlue = ab;
-                original[y][x].rgbtGreen = ag;
-            }
-        }
-    }
-    return;
 }
