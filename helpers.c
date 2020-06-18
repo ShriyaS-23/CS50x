832899985d36b1b07e0864f566502a52356dd1bf
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-06-18 16:03:56 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -72,8 +72,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width/2; j++)
         {
             RGBTRIPLE x = image[i][j];
-            image[i][j] = image[i][width - j];
-            image[i][width - j] = x;
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = x;
         }
     }
     return;
