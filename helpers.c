64e81b5f8069e99484164960151b19a55e36cfc5
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-06-18 15:25:48 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -5,7 +5,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width - 1; j++)
         {
             int b = image[i][j].rgbtBlue;
             int g = image[i][j].rgbtGreen;
