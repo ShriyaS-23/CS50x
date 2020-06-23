05bc6eceaf0e15ba650df8c1ff0fb7c3afdff653
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-06-23 14:48:03 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -92,9 +92,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-    for (int i = 0; i < height - 1; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width - 1; j++)
+        for(int j = 0; j < width; j++)
         {
             if (i == 0 && j == 0)//top left corner
          {
