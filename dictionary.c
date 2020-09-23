28fe0ad9586c7749105b33859a5f23e818dd22ea
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-09-23 14:12:11 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -23,7 +23,7 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 17577;
+const unsigned int N = 10000;
 
 // Hash table
 node *table[N];
@@ -34,33 +34,79 @@ int count = 0;
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    int h = hash(word);
-    
+    /*int h = hash(word);
+
     node *cursor = table[h];
-    
+
     while (cursor != NULL)
     {
         if (strcasecmp(cursor->word, word) == 0)
         {
             return true;
         }
-        
+
         cursor = cursor->next;
+    }
+
+    return false;*/
+    
+    int n = strlen(word);
+    char wordCopy[LENGTH + 1];
+    for (int i = 0; i < n; i++)
+    {
+        wordCopy[i] = tolower[word[i]];   
+    }
+    wordCopy[n] = '\0';
+    
+    int h = hash_index(wordCopy);
+    node *cursor = hashtable[h];
+    
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor->word, wordCopy) == 0)
+        {
+            return true;   
+        }
+        else
+        {
+            cursor = cursor->next;   
+        }
     }
     
     return false;
 }
 
 // Hashes word to a number
-unsigned int hash(const char *word)
+/*unsigned int hash(const char *word)
 {
-    char copy[3];
-    copy[0] = tolower(word[0]);
-    copy[1] = tolower(word[1]);
-    copy[2] = tolower(word[2]);
+    int x = strlen(word);
+    char copy[x];
 
-    int index = ((copy[0] - 65) * 676) + ((copy[1] - 65) * 26) + copy[2];
-    return index;
+    if (x-1 >= 3)
+    {
+        unsigned int index = (int)((copy[0] - 65) * 676) + ((copy[1] - 65) * 26) + (copy[2] - 65);
+            return index;
+    }
+    else if (x-1 == 2)
+    {
+        unsigned int index = (int)((copy[0] - 65) * 26) + (copy[1] - 65);
+        return index;
+    }
+    else
+    {
+        unsigned int index = (int)copy[0] - 65;
+        return index;
+    }
+}*/
+
+int hash_index(char *hash_this)
+{
+    unsigned int hash = 0;
+    for (int i = 0, n = strlen(hash_this); i < n; i++)
+    {
+        hash = (hash << 2) ^ hash_this[i];
+    }
+    return hash % N;
 }
 
 // Loads dictionary into memory, returning true if successful else false
@@ -71,21 +117,22 @@ bool load(const char *dictionary)
     {
         return false;
     }
-    
+
     char word [LENGTH+ 1];
-    
+
     while ((fscanf (dic, "%s", word)) != EOF)
     {
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
+            //unload; check if to do
             return false;
         }
-        
+
         strcpy (n->word, word);
-        
+
         int h = hash(n->word);
-        
+
         node *tmp = table[h];
         if (tmp == NULL)
         {
@@ -94,19 +141,23 @@ bool load(const char *dictionary)
         }
         else
         {
-            node *x = NULL;
+            /*node *x = NULL;
             while (tmp != NULL)
             {
                 x = tmp;
                 tmp = tmp->next;
             }
-            
+
             n = tmp;
             x->next = n;
+            count*/
+            
+            n->next = table[h];
+            table[h] = n;
             count ++;
         }
     }
-    
+
     fclose(dic);
     return true;
 }
@@ -120,10 +171,10 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    for (int i = 0; i < N; i++)
+    /*for (int i = 0; i < N; i++)
     {
         node *cursor = table[i];
-        
+
         while (cursor != NULL)
         {
             node *tmp = cursor;
@@ -131,6 +182,17 @@ bool unload(void)
             free(tmp);
         }
     }
+
+    return true;*/
     
+    node *tmp = NULL;
+    node *cursor = tmp;
+    
+    while(cursor != NULL)
+    {
+        node *x = cursor;
+        cursor = cursor->next;
+        free(x);
+    }
     return true;
 }
