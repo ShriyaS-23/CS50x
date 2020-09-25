5540722fc834524c26651d73248a5d0d4d698492
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-09-25 12:48:13 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,9 +1,12 @@
 
-h = int(input("Enter the Height:\n"))  # height input from user
+while True:
+    h = int(input("Enter the Height:"))  # height input from user
+    if h > 0 and h <= 8:
+        break
 
 for i in range(h):
     for c in range(i, h-1):  # printing space
         print(" ", end="")
-    for j in range(i):  # printing bricks
+    for j in range(i+1):  # printing bricks
         print("#", end="")
     print()  # moving to new line
