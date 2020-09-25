5f18f4517ee9939e5e42a6081d37f7160c4c58cd
ShriyaS-23 <ShriyaS-23@users.noreply.github.com>
2020-09-25 12:08:44 +0000

    automated commit by submit50

--- hello.py
+++ hello.py
@@ -1,2 +1,2 @@
-name = input("What is your name?\n")#asking for name input
-print(f"hello {name}")#printing message
+name = input("What is your name?\n")  # asking for name input
+print(f"hello {name}")  # printing message
